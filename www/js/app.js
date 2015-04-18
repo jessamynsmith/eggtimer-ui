6b31c11d7092db707ee808316ed112dcf422fc08
Jessamyn Smith <geekchick77@gmail.com>
2015-04-18 14:40:09 -0400

    Formatted app and controllers

--- www/js/app.js
+++ www/js/app.js
@@ -7,77 +7,77 @@
 // 'eggtimer.controllers' is found in controllers.js
 angular.module('eggtimer', ['ionic', 'eggtimer.controllers', 'eggtimer.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  // Each tab has its own nav history stack:
+  .config(function($stateProvider, $urlRouterProvider) {
 
-  .state('tab.calendar', {
-    url: '/calendar',
-    views: {
-      'tab-calendar': {
-        templateUrl: 'templates/tab-calendar.html',
-        controller: 'CalendarCtrl'
-      }
-    }
-  })
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  .state('tab.statistics', {
-      url: '/statistics',
-      views: {
-        'tab-statistics': {
-          templateUrl: 'templates/tab-statistics.html',
-          controller: 'StatisticsCtrl'
+      // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.calendar', {
+        url: '/calendar',
+        views: {
+          'tab-calendar': {
+            templateUrl: 'templates/tab-calendar.html',
+            controller: 'CalendarCtrl'
+          }
         }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/statistics/:chatId',
-      views: {
-        'tab-statistics': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+      })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+      .state('tab.statistics', {
+        url: '/statistics',
+        views: {
+          'tab-statistics': {
+            templateUrl: 'templates/tab-statistics.html',
+            controller: 'StatisticsCtrl'
+          }
+        }
+      })
+      .state('tab.chat-detail', {
+        url: '/statistics/:chatId',
+        views: {
+          'tab-statistics': {
+            templateUrl: 'templates/chat-detail.html',
+            controller: 'ChatDetailCtrl'
+          }
+        }
+      })
+
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/calendar');
+
   });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/calendar');
-
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,24 +1,24 @@
 angular.module('eggtimer.controllers', ['eggtimer.constants'])
 
-.controller('CalendarCtrl', function($scope, $ionicPopup, apiUrl) {
-  $scope.$on("$ionicView.loaded", function () {
-    initializeCalendar($ionicPopup, apiUrl);
+  .controller('CalendarCtrl', function($scope, $ionicPopup, apiUrl) {
+    $scope.$on("$ionicView.loaded", function() {
+      initializeCalendar($ionicPopup, apiUrl);
+    });
+  })
+
+  .controller('StatisticsCtrl', function($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+      Chats.remove(chat);
+    }
+  })
+
+  .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+  .controller('AccountCtrl', function($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
   });
-})
-
-.controller('StatisticsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
